0466e4b7be8222b95f6425f8b895cbbfb8bad76e
evargasf <118053278+evargasf@users.noreply.github.com>
2022-11-12 13:05:35 +0100

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,6 +5,6 @@ describe('sample render test', () => {
 
   it('displays the title text', () => {
     cy.get('h2')
-    .contains('AI MOVIE The Social network that create movies with artificial intelligence');
+    .contains('Essentials for a cold winter');
   })
 }) 
