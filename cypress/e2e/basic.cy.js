da4ac461fbe77517dfcd5fcf7f63c89b5657f862
evargasf <118053278+evargasf@users.noreply.github.com>
2022-11-12 13:09:25 +0100

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,6 +5,6 @@ describe('sample render test', () => {
 
   it('displays the title text', () => {
     cy.get('h2')
-    .contains('Esto no funciona');
+    .contains('Essentials for a cold winter');
   })
 }) 
