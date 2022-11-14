35d33d591fff5dfea8efbd56bec202d41621d538
evargasf <118053278+evargasf@users.noreply.github.com>
2022-11-14 12:38:43 +0100

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,87 @@
-<Hero
-  maxWidth='500px' // how big the image's maxumim should be
-  image={'/banner1.png'} // the source location for the image
-  title={'AI MOVIE'} // the main text displayed
-  subtitle={'Discover Autumn Winter 2021'} // text found below the main text
-  ctaText={'shop now'} // the presented text for a user to click on
-  ctaAction={goToShop} // the location the call-to-action text directs users
-/>
+import React from 'react';
+
+import * as styles from './Hero.module.css';
+
+import Button from '../Button';
+
+import { Link } from 'gatsby';
+
+
+
+const Hero = (props) => {
+
+  const {
+
+    title,
+
+    subtitle,
+
+    ctaText,
+
+    ctaAction,
+
+    image,
+
+    maxWidth,
+
+    ctaStyle,
+
+    ctaLink,
+
+    ctaTo,
+
+    header,
+
+  } = props;
+
+  return (
+
+    <div className={styles.root} style={{ backgroundImage: `url(${image})` }}>
+
+      <div className={styles.content} style={{ maxWidth: maxWidth }}>
+
+        {header && <span className={styles.header}>{header}</span>}
+
+        {title && <h2 className={styles.title}>{title}</h2>}
+
+        {subtitle && <span className={styles.subtitle}>{subtitle}</span>}
+
+        {ctaText && (
+
+          <Button
+
+            className={`${styles.ctaButton} ${ctaStyle}`}
+
+            level={'primary'}
+
+            onClick={ctaAction}
+
+          >
+
+            {ctaText}
+
+          </Button>
+
+        )}
+
+        {ctaLink && (
+
+          <Link className={styles.ctaLink} to={ctaTo}>
+
+            {ctaLink}
+
+          </Link>
+
+        )}
+
+      </div>
+
+    </div>
+
+  );
+
+};
+
+
+
+
