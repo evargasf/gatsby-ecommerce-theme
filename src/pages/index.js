a656931cd69ee4f1e33617e67dd3dd58e0c1cb0c
evargasf <118053278+evargasf@users.noreply.github.com>
2022-11-14 11:10:29 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
+        title={'AI-MOVIE'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'Shop now'}
         ctaAction={goToShop}
